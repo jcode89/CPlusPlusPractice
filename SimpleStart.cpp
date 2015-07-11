7ead0b6d6a88c1bb34657bfa7994a33852b98ab8
JCode89 <JV.Colon@Yahoo.com>
2015-07-10 22:35:12 -0400

    Initial Commit.

---
+++ SimpleStart.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+// Calling this allows us to just use cout instead of having to call std::cout
+using namespace std;
+
+//main() is where program execution begins
+// All programs run from main
+int main()
+{
+	cout << "Hello World! "; // Prints out the statement
+	cout << "Very very simple program! ";
+	cout << "But I need to start from here to refresh my memory.";
+	return 0;
+}
