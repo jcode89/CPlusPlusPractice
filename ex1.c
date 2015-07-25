b01b850a573d1fa8f3e2aa146318424414d02870
Jcode89 <JV.Colon@yahoo.com>
2015-07-25 02:35:45 -0400

    LCTHWs First Example.

---
+++ ex1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    puts("Hello world.");
+    puts("More Text to print in C!");
+    puts("This isn't so bad right now.");
+    puts("A little different than C++ though."); 
+    return 0;
+}
