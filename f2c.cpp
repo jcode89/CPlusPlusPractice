877c79058057fd756ab1b53379679e0a6788ee53
JCode89 <JV.Colon@Yahoo.com>
2015-08-03 18:51:33 -0400

    Adds Hungarian notation.

--- f2c.cpp
+++ f2c.cpp
@@ -2,20 +2,22 @@
 using namespace std;
 // A function  containing the code that converts Fahrenheit to Celsius.
 // placing double in front of the function name, lets the compiler know that a double is being returned.
-double converter(double f)
+// double dC << This is Hungarian notation. d == double.
+double converter(double dF)
 {
-	double c = (f-32)* 5/9;
-	return c;
+    double dC;
+	dC = (dF-32)* 5/9;
+	return dC;
 }
 int main()
 {
 	// Variable declared so that we can enter a value
 	// save the value
 	// then place it within our function and return the answer.
-	double a;
+	double dA;
 	cout << "Enter degrees in Fahrenheit: ";
-	cin >> a;
+	cin >> dA;
 	// The function can easily be called and executed in the print statement.
 	// And remember everything runs through main.
-	cout << "The degrees in Celsius are: " << converter(a) << endl;
+	cout << "The degrees in Celsius are: " << converter(dA) << endl;
 }
