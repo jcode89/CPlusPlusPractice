38c56e09bee876ea84c4b08843c8655e735823c6
Jcode89 <JV.Colon@yahoo.com>
2015-08-04 23:55:45 -0400

    A simple if/else example.

---
+++ ifEx1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    int iX;
+    cout << "Enter a number: ";
+    cin >> iX;
+
+    if(iX == 100){
+        cout << "iX is 100" << "\n";
+        cout << "See " << iX << endl;
+    }
+    else{
+        cout << "iX is definitely not 100!" << "\n;
+        cout << "iX is " << iX << endl;
+    }
+}
